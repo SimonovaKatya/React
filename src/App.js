ec461249bbdf1bef2da74692008ea029ebe9e3dc
Ekaterina Simonova <simonovaekaterina227@mail.ru>
2020-07-28 17:17:20 +0300

    removed

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit and save to reload.
+                </p>
+                <p>
+                    Edit and save to reload.
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
