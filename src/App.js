9b28a7d9c29abef5500e6b8120d09d57a11f15ed
Ekaterina Simonova <simonovaekaterina227@mail.ru>
2020-08-02 03:57:20 +0300

    сreated the Profile component. Made templates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Profile from "./Components/Profile/Profile";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit and save to reload.
-                </p>
+               <Profile/>
                 <p>
                     Edit and save to reload.
                 </p>
---
+++ src/Components/Profile/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Profile.module.css';
+
+const Profile = () => {
+    return (
+        <div>
+             Post1
+             Post2
+             Post3
+        </div>
+    )
+}
+
+export default Profile;
