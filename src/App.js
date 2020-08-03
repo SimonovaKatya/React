60de0d5354aa7ea4c66a238beab44c467fd6a36d
Ekaterina Simonova <simonovaekaterina227@mail.ru>
2020-08-03 20:20:34 +0300

    Created a component profile.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
-import Profile from "./Components/Profile/Profile";
+import Header from "./COMPONENTS/Header/Header";
+import Navbar from "./COMPONENTS/Navbar/Navbar";
+import Profile from "./COMPONENTS/Profile/Profile";
+import Dialogs from "./COMPONENTS/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import News from "./COMPONENTS/News/News";
+import Music from "./COMPONENTS/Music/Music";
+import Settings from "./COMPONENTS/Settings/Settings";
 
-function App() {
+
+/*const L = [
+    {name: 'mark', id: 1},
+    {name: 'katy', id: 3},
+    {name: 'petia', id: 4},
+    {name: 'max', id: 7},
+]*/
+
+const App = () => {
+    /* const [isRed, setIsRed] = useState(true)*/
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-               <Profile/>
-                <p>
-                    Edit and save to reload.
-                </p>
-            </header>
-        </div>
+        <BrowserRouter>
+
+
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                {/*  <NavBar1 list={L} isRed={isRed}/>*/}
+                {/*   <button onClick={() => setIsRed(!isRed)}>сменить цвет</button>*/}
+                <div className='app-wrapper-content'>
+                    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/settings' component={Settings}/>
+                </div>
+            </div>
+        </BrowserRouter>
     );
 }
+
+/*const returnColor = (isRed) => isRed ? {background: "red"} : {background: "blue"}
+
+const NavBar1 = ({list, isRed}) => [list.filter(item => item.name !== 'petia').map(item => <div style={returnColor(isRed)} key={item.id}>{item.name}</div>)]*/
+
 
 export default App;
--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -7,6 +7,18 @@ const Profile = () => {
              Post1
              Post2
              Post3
+             Post3
+             Post3
+             Post3
+             Post3
+             Post3
+             Post3
+             Post3
+             Post3
+             Post3
+             Post3
+             Post3
+             Post3dd
         </div>
     )
 }
